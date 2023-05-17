0a3075c1e179b5b05929355eb00fef6a5b239213
Yasmin khan <yasmin.khan@ordextechnology.com>
2023-05-17 16:40:01 +0530

    Update appcenter-pre-build.sh

--- appcenter-pre-build.sh
+++ appcenter-pre-build.sh
@@ -6,3 +6,10 @@ npm install --save-dev jest
 # Run tests
 npm run test-ci
 
+# Check if all tests passed
+if [ $? -eq 0 ]; then
+  # Build the app
+  npm run build
+else
+  echo "Tests failed. Skipping the build."
+fi
